e756831457189c6364461816227a254c49dd358c
yh515511038 <yh515511038@gmail.com>
2024-05-28 03:22:45 +0000

    change App text

--- src/App.js
+++ src/App.js
@@ -9,13 +9,16 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+	<p>
+	  Changed on Feature branch...
+	</p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-	  learn react
+	  learn react</br>
           Hello world React!!!
         </a>
       </header>
