8c7471f43cd2c7522eb1d2165d94f7f46caf652f
yh515511038 <yh515511038@gmail.com>
2024-05-28 03:28:45 +0000

    changed App text

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-	  learn react</br>
+	  learn react
           Hello world React!!!
         </a>
       </header>
